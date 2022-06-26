1c41a215a12ed4ff1e046f64d97241339a2878a4
DIEGO MALTA GOUVEIA <diego.pel@hotmail.com>
2022-06-26 20:04:05 -0300

    upload index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bem Me Quero Studio</title>
+    <!-- links de estilo -->
+    <?php require("css/links-head.php"); ?>
+</head>
+<body>
+    <?php require("requires/menu-top.php"); ?>
+
+    <!-- header -->
+    <?php require("requires/header.php"); ?>
+
+    <!-- Serviços -->
+    <?php require("requires/services.php"); ?>
+
+    <!-- Sobre nós -->
+    <?php require("requires/about.php"); ?>
+
+    <!-- Estilo Perfeito -->
+    <?php require("requires/perfectstyle.php"); ?>
+
+    <!-- Profissionais -->
+    <?php require("requires/team.php"); ?>
+
+    <!-- Galeria de fotos -->
+    <?php require("requires/gallery.php"); ?>
+
+    <!-- Comentários dos Clientes -->
+    <?php require("requires/testimonials.php"); ?>
+
+    <!-- Contate nos -->
+    <?php require("requires/contact.php"); ?>
+
+    <!-- rodape/footer -->
+    <?php require("requires/footer.php"); ?>
+
+    <!-- links de estilo js e script Tawk.to -->
+    <?php require("css/links-footer.php"); ?>
+</body>
+</html>
